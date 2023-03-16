145391c0b61ff13d592aa04a268e43955c10d636
MikaSchulz <msz@fam-schulz-hamburg.de>
2023-03-16 21:22:33 +0100

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:src/main/java/me/eyetealer/mckingofthehill/kingofthehill/KingOfTheHill.java

--- src/main/java/me/eyetealer/mckingofthehill/kingofthehill/KingOfTheHill.java
+++ src/main/java/me/eyetealer/mckingofthehill/kingofthehill/KingOfTheHill.java
@@ -1,26 +1,37 @@
 package me.eyetealer.mckingofthehill.kingofthehill;
 
+import static net.kyori.adventure.text.minimessage.MiniMessage.miniMessage;
+
+import cloud.commandframework.ArgumentDescription;
 import cloud.commandframework.Command;
 import cloud.commandframework.CommandTree;
 import cloud.commandframework.annotations.AnnotationParser;
 import cloud.commandframework.arguments.parser.ParserParameters;
 import cloud.commandframework.arguments.parser.StandardParameters;
+import cloud.commandframework.arguments.standard.IntegerArgument;
 import cloud.commandframework.bukkit.BukkitCommandManager;
 import cloud.commandframework.bukkit.CloudBukkitCapabilities;
+import cloud.commandframework.bukkit.parsers.MaterialArgument;
 import cloud.commandframework.execution.CommandExecutionCoordinator;
 import cloud.commandframework.execution.FilteringCommandSuggestionProcessor;
 import cloud.commandframework.meta.CommandMeta;
 import cloud.commandframework.paper.PaperCommandManager;
+import cloud.commandframework.types.tuples.Triplet;
+import io.leangen.geantyref.TypeToken;
+import java.util.List;
 import java.util.function.Function;
 import lombok.Getter;
-import me.eyetealer.mckingofthehill.kingofthehill.command.CommandHandler;
 import me.eyetealer.mckingofthehill.kingofthehill.configuration.ConfigProvider;
-import me.eyetealer.mckingofthehill.kingofthehill.configuration.PluginProperties;
 import me.eyetealer.mckingofthehill.kingofthehill.database.sql.SqlInitializer;
 import me.eyetealer.mckingofthehill.kingofthehill.gamestate.GameState;
 import me.eyetealer.mckingofthehill.kingofthehill.gamestate.StateHandler;
+import me.eyetealer.mckingofthehill.kingofthehill.utils.Messages;
+import org.bukkit.Material;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.util.Vector;
 
 public class KingOfTheHill extends JavaPlugin {
 
@@ -41,8 +52,8 @@ public class KingOfTheHill extends JavaPlugin {
 
     testCloudCommand();
 
-    CommandHandler commandHandler = new CommandHandler(plugin);
-    commandHandler.init();
+//    CommandHandler commandHandler = new CommandHandler(plugin);
+//    commandHandler.init();
 
     StateHandler stateHandler = new StateHandler(plugin);
 
@@ -51,42 +62,24 @@ public class KingOfTheHill extends JavaPlugin {
 
   private void testCloudCommand() {
 
-     final Function<CommandTree<CommandSender>, CommandExecutionCoordinator<CommandSender>> executionCoordinatorFunction =
-            CommandExecutionCoordinator.simpleCoordinator();
-    //
-    // This function maps the command sender type of our choice to the bukkit command sender.
-    // However, in this example we use the Bukkit command sender, and so we just need to map it
-    // to itself
-    //
-    final Function<CommandSender, CommandSender> mapperFunction = Function.identity();
+    final Function<CommandTree<CommandSender>, CommandExecutionCoordinator<CommandSender>> executionCoordinatorFunction =
+        CommandExecutionCoordinator.simpleCoordinator();
     try {
-      this.manager = new PaperCommandManager<>(
-          /* Owning plugin */ this,
-          /* Coordinator function */ executionCoordinatorFunction,
-          /* Command Sender -> C */ mapperFunction,
-          /* C -> Command Sender */ mapperFunction
-      );
+      this.manager = PaperCommandManager.createNative(this, executionCoordinatorFunction);
     } catch (final Exception e) {
       this.getLogger().severe("Failed to initialize the command this.manager");
-      /* Disable the plugin */
       this.getServer().getPluginManager().disablePlugin(this);
       return;
     }
 
-    // Use contains to filter suggestions instead of default startsWith
     this.manager.commandSuggestionProcessor(new FilteringCommandSuggestionProcessor<>(
-        FilteringCommandSuggestionProcessor.Filter.<CommandSender>contains(true).andTrimBeforeLastSpace()
+        FilteringCommandSuggestionProcessor.Filter.<CommandSender>contains(true)
+            .andTrimBeforeLastSpace()
     ));
 
-    //
-    // Register Brigadier mappings
-    //
     if (this.manager.hasCapability(CloudBukkitCapabilities.BRIGADIER)) {
       this.manager.registerBrigadier();
     }
-    //
-    // Register asynchronous completions
-    //
     if (this.manager.hasCapability(CloudBukkitCapabilities.ASYNCHRONOUS_COMPLETION)) {
       ((PaperCommandManager<CommandSender>) this.manager).registerAsynchronousCompletions();
     }
@@ -99,33 +92,57 @@ public class KingOfTheHill extends JavaPlugin {
             // This will allow you to decorate commands with descriptions
             .with(CommandMeta.DESCRIPTION, p.get(StandardParameters.DESCRIPTION, "No description"))
             .build();
-    this.annotationParser = new AnnotationParser<>(
-        /* Manager */ this.manager,
-        /* Command sender type */ CommandSender.class,
-        /* Mapper for command meta instances */ commandMetaFunction
-    );
-    //
-    // Create the commands
-    //
+    this.annotationParser = new AnnotationParser<>(this.manager, CommandSender.class,
+        commandMetaFunction);
+
     this.constructCommands();
 
   }
 
   private void constructCommands() {
-    //
-    // Parse all @CommandMethod-annotated methods
-    //
     this.annotationParser.parse(this);
-    // Parse all @CommandContainer-annotated classes
     try {
       this.annotationParser.parseContainers();
     } catch (final Exception e) {
       e.printStackTrace();
     }
-    //
-    // Base command builder
-    //
-    final Command.Builder<CommandSender> builder = this.manager.commandBuilder("example");
+    List<Command.Builder<CommandSender>> builderList = List.of(
+        manager.commandBuilder("kingofthehill"),
+        manager.commandBuilder("koth")
+    );
+
+    builderList.forEach(builder -> {
+
+      this.manager.command(builder.literal("setup")
+              .literal("me")
+              .senderType(Player.class)
+              .argumentTriplet(
+                  "coords",
+                  TypeToken.get(Vector.class),
+                  Triplet.of("x", "y", "z"),
+                  Triplet.of(Integer.class, Integer.class, Integer.class),
+                  (sender, triplet) -> new Vector(triplet.getFirst(), triplet.getSecond(),
+                      triplet.getThird()
+                  ),
+                  ArgumentDescription.of("Coordinates")
+              )
+              .handler(context -> this.manager.taskRecipe().begin(context)
+                  .synchronous(commandContext -> {
+                    final Player player = (Player) commandContext.getSender();
+                    player.sendMessage(Messages.join(miniMessage().deserialize("TEST123")));
+                  }).execute()))
+          .command(builder.literal("give")
+              .senderType(Player.class)
+              .argument(MaterialArgument.of("material"))
+              .argument(IntegerArgument.of("amount"))
+              .handler(c -> {
+                final Material material = c.get("material");
+                final int amount = c.get("amount");
+                final ItemStack itemStack = new ItemStack(material, amount);
+                ((Player) c.getSender()).getInventory().addItem(itemStack);
+                c.getSender().sendMessage("You've been given stuff, bro.");
+              }));
+    });
   }
 
   public void onDisable() {
